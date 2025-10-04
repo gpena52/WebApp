662cb4eea221184c1199847f4d8a82aa3fc9879d
gpena52 <g.pena52@unapec.edu.do>
2025-10-04 03:30:15 -0400

    first commit

---
+++ WebApp_APPA00114138/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp_APPA00114138.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
